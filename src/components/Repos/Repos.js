153ded1cd8f741c541cd8fabc4c631cf73198505
Raghu Ganapathy <raghu.1999@gmail.com>
2020-07-06 22:22:41 +0530

    deployment 1

--- src/components/Repos/Repos.js
+++ src/components/Repos/Repos.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { Segment, List, Header, Divider } from 'semantic-ui-react';
 import Repo from './Repo';
 function Repos({ repos }) {
+  console.log(repos.length);
   return (
-    <>
-      <Segment padded='very'>
-        <Header as='h3'>Public Repos</Header>
-        <List horizontal>
-          {repos.map((repo) => (
-            <Repo key={repo.id} repo={repo} />
-          ))}
-        </List>
-      </Segment>
+    <div>
+      {repos.length > 0 && (
+        <Segment padded='very'>
+          <Header as='h3'>Public Repos</Header>
+          <List horizontal>
+            {repos.map((repo) => (
+              <Repo key={repo.id} repo={repo} />
+            ))}
+          </List>
+        </Segment>
+      )}
       <Divider hidden />
       <Divider hidden />
-    </>
+    </div>
   );
 }
 
